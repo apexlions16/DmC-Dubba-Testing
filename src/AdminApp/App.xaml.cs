9ff60e865c979123c6f145274942e105b553a38f
apexlions16 <hcywashere@gmail.com>
2026-08-10 14:40:32 +0300

    Admin uygulamasında Türkçe kültürü etkinleştir

--- src/AdminApp/App.xaml.cs
+++ src/AdminApp/App.xaml.cs
@@ -1,7 +1,12 @@
 using System.Windows;
+using DmC.Qa.Shared;
 
 namespace DmC.Qa.Admin;
 
 public partial class App : Application
 {
+    public App()
+    {
+        TurkishUi.ApplyCulture();
+    }
 }
