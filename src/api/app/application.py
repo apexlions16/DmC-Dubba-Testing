11c8953f0903c6b206738f6a8b45345087e5ee31
apexlions16 <hcywashere@gmail.com>
2026-08-10 14:51:03 +0300

    Video kanıt API'sini uygulamaya kaydet

--- src/api/app/application.py
+++ src/api/app/application.py
@@ -2,6 +2,7 @@ from . import main as main_module
 from .auth import get_current_user as get_device_current_user
 from .auth_api import router as auth_router
 from .build_api import router as build_router
+from .evidence_api import router as evidence_router
 from .release_api import router as release_router
 
 app = main_module.app
@@ -13,6 +14,7 @@ app.dependency_overrides[main_module.get_current_user] = get_device_current_user
 
 app.include_router(auth_router)
 app.include_router(build_router)
+app.include_router(evidence_router)
 app.include_router(release_router)
 
 __all__ = ["app"]
