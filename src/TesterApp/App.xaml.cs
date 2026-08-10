f677b93e7e62ee5822b2af2398918cf2d0c5a03f
apexlions16 <hcywashere@gmail.com>
2026-08-10 14:40:24 +0300

    Tester uygulamasında Türkçe kültürü etkinleştir

--- src/TesterApp/App.xaml.cs
+++ src/TesterApp/App.xaml.cs
@@ -1,7 +1,12 @@
 using System.Windows;
+using DmC.Qa.Shared;
 
 namespace DmC.Qa.Tester;
 
 public partial class App : Application
 {
+    public App()
+    {
+        TurkishUi.ApplyCulture();
+    }
 }
