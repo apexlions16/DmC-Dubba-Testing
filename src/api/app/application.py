39f71b6ffbacacdd2668dd03d0413124cd1b2565
apexlions16 <hcywashere@gmail.com>
2026-08-10 16:05:00 +0300

    Güvenli purge routerını platform API'sine bağla

--- src/api/app/application.py
+++ src/api/app/application.py
@@ -10,6 +10,7 @@ from .build_api import router as build_router
 from .evidence_api import router as evidence_router
 from .local_test import ensure_local_test_seed
 from .notification_api import router as notification_router
+from .purge_api import router as purge_router
 from .release_api import router as release_router
 from .retest_api import router as retest_router
 from .tester_api import router as tester_router
@@ -25,6 +26,7 @@ app.include_router(admin_router)
 app.include_router(build_router)
 app.include_router(evidence_router)
 app.include_router(notification_router)
+app.include_router(purge_router)
 app.include_router(retest_router)
 app.include_router(release_router)
 app.include_router(tester_router)
