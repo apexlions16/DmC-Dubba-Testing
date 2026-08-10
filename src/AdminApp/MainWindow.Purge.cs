aa9323a01e1a4130684701c412511705e1d57540
apexlions16 <hcywashere@gmail.com>
2026-08-10 20:55:39 +0300

    Admin partial OnContentRendered çakışmasını kaldır ve purge davranışını koru

--- src/AdminApp/MainWindow.Purge.cs
+++ src/AdminApp/MainWindow.Purge.cs
@@ -4,12 +4,6 @@ namespace DmC.Qa.Admin;
 
 public partial class MainWindow
 {
-    protected override void OnContentRendered(EventArgs e)
-    {
-        base.OnContentRendered(e);
-        AttachButton("Kalıcı Silme Talebi Oluştur", (_, _) => OpenPurgeWindow());
-    }
-
     private void OpenPurgeWindow()
     {
         if (!IsAdminRole(_signedInUser.Role))
