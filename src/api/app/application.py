fa9f4659488d421023a823b6e8f642f2796b2a02
apexlions16 <hcywashere@gmail.com>
2026-08-10 14:23:19 +0300

    Register build file API

--- src/api/app/application.py
+++ src/api/app/application.py
@@ -1,6 +1,8 @@
+from .build_api import router as build_router
 from .main import app
 from .release_api import router as release_router
 
+app.include_router(build_router)
 app.include_router(release_router)
 
 __all__ = ["app"]
